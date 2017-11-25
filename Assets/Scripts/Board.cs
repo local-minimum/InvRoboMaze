0b66f0dde3b7c1e1a88f0b18573f44b0d3635b71
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-11-25 22:10:11 +0100

    Assets

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public int players = 2;
+
+    public int boardSize = 7;
+
+    [SerializeField]
+    Tile _prefab;
+
+    public float tileSize = 1.05f;
+
+    List<Tile> tiles = new List<Tile>();
+
+	void Start () {
+	    if (boardSize % 2 == 0)
+        {
+            boardSize++;
+            Debug.LogWarning("Increased board size because it must be odd");
+        }
+        SetupTiles();
+	}
+
+	void SetupTiles()
+    {
+        int halfSize = (boardSize - 1) / 2;
+
+        for (int row = 0; row < boardSize; row ++)
+        {
+            for (int col = 0; col < boardSize; col ++)
+            {
+                Vector3 pos = new Vector3((row - halfSize) * tileSize, 0, (col - halfSize) * tileSize);
+                Tile tile = Instantiate(_prefab, pos, Quaternion.identity, transform);
+
+                tile.Setup(
+                    (row % 2) == 1 && (col % 2) == 1 ? TileType.Swapping : TileType.Sliding,
+                    row,
+                    col);
+
+                tile.onMove += Tile_onMove;
+                tile.onMoveEnd += Tile_onMoveEnd;
+                tile.onMoveStart += Tile_onMoveStart;
+
+                tiles.Add(tile);
+            }
+        }
+    }
+
+    bool sourceCanMove;
+    int sourceRow;
+    int sourceCol;
+    Tile source;
+    Tile target;
+    Vector3 moveOffset;
+    Tile[] moveRow = new Tile[0];
+    Tile[] moveCol = new Tile[0];
+    float noSlideThreshold = 0.33f;
+    bool canSlideRow;
+    bool canSlideCol;
+    bool canSlide;
+
+    private void Tile_onMoveStart(int row, int col)
+    {
+        source = GetTile(row, col);
+        sourceCanMove = source.canSlide || source.canSwap;
+        sourceRow = row;
+        sourceCol = col;
+        source.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        if (source.canSlide)
+        {
+            moveRow = GetRowTiles(row);
+            moveCol = GetColTiles(col);
+            canSlideCol = CanSlide(moveCol);
+            canSlideRow = CanSlide(moveRow);
+            canSlide = canSlideCol || canSlideRow;
+        }
+    }
+
+    private void Tile_onMoveEnd()
+    {
+        SetNewTilePositions();
+        ClearMoveSource();
+        if (source != null)
+        {
+            source.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+        
+    }
+
+    void SetNewTilePositions()
+    {
+ 
+    }
+
+    void ClearMoveSource()
+    {
+        sourceCanMove = false;
+        sourceRow = 0;
+        sourceCol = 0;
+        if (canSlide)
+        {
+            ResetSliding(moveCol);
+            ResetSliding(moveRow);
+        }
+        moveRow = new Tile[0];
+        moveCol = new Tile[0];
+        canSlideRow = false;
+        canSlideCol = false;
+        canSlide = false;
+    }
+
+    private void Tile_onMove(Vector3 offset)
+    {
+        moveOffset = offset;
+        if (canSlide)
+        {
+            bool slideIsRow = Mathf.Abs(offset.x) < Mathf.Abs(offset.z);
+
+            if (slideIsRow && canSlideRow && Mathf.Abs(offset.z) > noSlideThreshold * tileSize)
+            {
+                Sliding(moveRow);
+                ResetSliding(moveCol);
+            }
+            else if (!slideIsRow && canSlideCol && Mathf.Abs(offset.x) > noSlideThreshold * tileSize)
+            {
+                Sliding(moveCol);
+                ResetSliding(moveRow);
+            }
+        }
+    }
+
+    void Sliding(Tile[] tiles)
+    {
+        for(int i=0; i<tiles.Length; i++)
+        {
+            Tile t = tiles[i];
+            if (t != source)
+            {
+                t.GetComponent<MeshRenderer>().material.color = Color.magenta;
+            }
+        }
+    }
+
+    void ResetSliding(Tile[] tiles)
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            Tile t = tiles[i];
+            if (t != source)
+            {
+                t.GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+    }
+
+    public Tile GetTile(int row, int col)
+    {
+        foreach(Tile t in tiles)
+        {
+            if (t.hasPosition(row, col))
+            {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public Tile[] GetRowTiles(int row)
+    {
+        Tile[] tiles = new Tile[boardSize];
+        int curTile = 0;
+
+        foreach(Tile tile in this.tiles)
+        {
+            if (tile.isOnRow(row))
+            {
+                tiles[curTile] = tile;
+                curTile++;
+            }
+        }
+        return tiles;
+    }
+
+    public Tile[] GetColTiles(int row)
+    {
+        Tile[] tiles = new Tile[boardSize];
+        int curTile = 0;
+
+        foreach (Tile tile in this.tiles)
+        {
+            if (tile.isOnCol(row))
+            {
+                tiles[curTile] = tile;
+                curTile++;
+            }
+        }
+        return tiles;
+    }
+
+    public static bool CanSlide(Tile[] tiles)
+    {
+        for (int i=0; i<tiles.Length; i++)
+        {
+            if (!tiles[i].canSlide)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType { Sliding, Swapping, Stationary };
+
+public delegate void MoveEvent(Vector3 offset);
+public delegate void MoveStartEvent(int row, int col);
+public delegate void MoveEndEvent();
+
+
+public class Tile : MonoBehaviour {
+
+    [SerializeField, HideInInspector]
+    TileType tileType = TileType.Sliding;
+
+    [SerializeField, HideInInspector]
+    int row;
+
+    [SerializeField, HideInInspector]
+    int col;
+
+    public event MoveEvent onMove;
+    public event MoveStartEvent onMoveStart;
+    public event MoveEndEvent onMoveEnd;
+
+    public void Setup(TileType tileType, int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+        this.tileType = tileType;
+        name = string.Format("Tile ({0}, {1}) [{2}]", row, col, tileType);
+    }
+
+    public bool hasPosition(int row, int col)
+    {
+        return this.row == row && this.col == col;
+    }
+
+    public bool isOnRow(int row)
+    {
+        return this.row == row;
+    }
+
+    public bool isOnCol(int col)
+    {
+        return this.col == col;
+    }
+
+    public bool canSlide
+    {
+        get
+        {
+            return tileType == TileType.Sliding;
+        }
+    }
+
+    public bool canSwap
+    {
+        get
+        {
+            return tileType == TileType.Sliding;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool moving = false;
+    Vector3 moveSource;
+
+    private void OnMouseDrag()
+    {
+        if (!moving)
+        {
+            moving = true;
+            moveSource = mousePointOnPlane();
+            if (onMoveStart != null)
+            {
+                onMoveStart(row, col);
+            }
+        }
+
+        Vector3 currentPosition = mousePointOnPlane();
+        Vector3 moveOffset = currentPosition - moveSource;
+
+        if (onMove != null)
+        {
+            onMove(moveOffset);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+        if (onMoveEnd != null)
+        {
+            onMoveEnd();
+        }
+    }
+
+    Vector3 mousePointOnPlane()
+    {
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane p = new Plane(transform.up * -1, 0.1f);
+        float t;
+        if (p.Raycast(r, out t))
+        {
+            return r.GetPoint(t);
+        }
+        throw new System.InvalidOperationException("Camera should look at board at all times");
+    }
+}

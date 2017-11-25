64235485efc096160c0b641c6c6b2f8df005f6f3
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-11-25 23:40:49 +0100

    Beginnings of a player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    Color color;
+
+	void Start () {
+        SetColor();
+	}
+	
+    void SetColor()
+    {
+        foreach(MeshRenderer rend in GetComponentsInChildren<MeshRenderer>())
+        {
+            rend.material.color = color;
+        }
+    }
+}
